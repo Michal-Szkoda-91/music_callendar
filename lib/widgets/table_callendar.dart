97bbb5a300c04bbb841ce9cbd1f0c0801e95207b
Michal Szkoda <78780@stud.uz.zgora.pl>
2021-07-31 21:00:42 +0200

    utworzono przycisk dodawania eventu

--- lib/widgets/table_callendar.dart
+++ lib/widgets/table_callendar.dart
@@ -7,8 +7,7 @@ import '../builders/selected_day_builder.dart';
 import '../builders/today_builder.dart';
 
 class CustomTableCalendar extends StatefulWidget {
-  CustomTableCalendar({Key? key, required this.firstDay}) : super(key: key);
-  final String firstDay;
+  CustomTableCalendar({Key? key}) : super(key: key);
 
   @override
   _CustomTableCalendarState createState() => _CustomTableCalendarState();
@@ -17,53 +16,61 @@ class CustomTableCalendar extends StatefulWidget {
 class _CustomTableCalendarState extends State<CustomTableCalendar> {
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
-  late DateTime _startDate;
 
   @override
   Widget build(BuildContext context) {
-    _startDate = DateTime.parse(widget.firstDay);
-    return TableCalendar(
-      locale: 'pl_PL',
-      calendarFormat: CalendarFormat.month,
-      headerStyle: HeaderStyle(
-        formatButtonVisible: false,
-      ),
-      startingDayOfWeek: StartingDayOfWeek.monday,
-      focusedDay: _focusedDay,
-      firstDay: DateTime.utc(2000),
-      lastDay: DateTime.utc(2200),
-      calendarStyle: CalendarStyle(
-        outsideDaysVisible: false,
-      ),
-      holidayPredicate: (day) {
-        if (day.weekday == 6 || day.weekday == 7) {
-          return true;
-        } else
-          return false;
-      },
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        setState(() {
-          _selectedDay = selectedDay;
-          _focusedDay = focusedDay;
-        });
-      },
-      calendarBuilders: CalendarBuilders(
-        selectedBuilder: (context, day, today) {
-          return SelectedDayBuilder(day: day);
-        },
-        todayBuilder: (context, day, today) {
-          return TodayBuilder(day: day);
-        },
-        holidayBuilder: (context, day, today) {
-          return HolidayBuilder(day: day);
-        },
-        defaultBuilder: (context, day, today) {
-          return DefaultBuilder(day: day);
-        },
-      ),
+    return Column(
+      children: [
+        TableCalendar(
+          availableGestures: AvailableGestures.horizontalSwipe,
+          locale: 'pl_PL',
+          calendarFormat: CalendarFormat.month,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+          ),
+          startingDayOfWeek: StartingDayOfWeek.monday,
+          focusedDay: _focusedDay,
+          firstDay: DateTime.utc(2000),
+          lastDay: DateTime.utc(2200),
+          calendarStyle: CalendarStyle(
+            outsideDaysVisible: false,
+          ),
+          holidayPredicate: (day) {
+            if (day.weekday == 6 || day.weekday == 7) {
+              return true;
+            } else
+              return false;
+          },
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          },
+          calendarBuilders: CalendarBuilders(
+            selectedBuilder: (context, day, today) {
+              return SelectedDayBuilder(day: day);
+            },
+            todayBuilder: (context, day, today) {
+              return TodayBuilder(day: day);
+            },
+            holidayBuilder: (context, day, today) {
+              return HolidayBuilder(day: day);
+            },
+            defaultBuilder: (context, day, today) {
+              return DefaultBuilder(day: day);
+            },
+          ),
+        ),
+        TextButton.icon(
+          onPressed: () {},
+          icon: Icon(Icons.headphones),
+          label: Text("Rozpocznij grę"),
+        ),
+      ],
     );
   }
 }
