2bbcca82a95f837be0e41c529467e511d31637df
Michal Szkoda <78780@stud.uz.zgora.pl>
2021-08-03 16:39:48 +0200

    dodano przycis zapisu oraz pytanie przy powrocie

--- lib/screens/music_event_adding_screen.dart
+++ lib/screens/music_event_adding_screen.dart
@@ -26,8 +26,14 @@ class _MusicEventAddingScreenState extends State<MusicEventAddingScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          iconTheme: IconThemeData(
-            color: Theme.of(context).textTheme.headline1!.color,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Theme.of(context).textTheme.headline1!.color,
+            ),
+            onPressed: () {
+              _showExitQuestion(context);
+            },
           ),
           title: Text(
             _appBarDate,
@@ -42,19 +48,84 @@ class _MusicEventAddingScreenState extends State<MusicEventAddingScreen> {
             children: [
               TargetTimeRow(),
               PlayTimeRow(),
+              //Tutaj bedzie ekualizer
               Card(
-                margin: EdgeInsets.symmetric(horizontal: 20),  
+                margin: EdgeInsets.symmetric(horizontal: 20),
                 child: Container(
                   width: double.infinity,
                   height: 200,
                 ),
                 color: Theme.of(context).primaryColor,
-              )
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton.icon(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).primaryColor,
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).accentColor,
+                  ),
+                ),
+                onPressed: () {},
+                icon: Icon(Icons.save),
+                label: Text("Zapisz wynik"),
+              ),
             ],
           ),
         ),
         floatingActionButton: NoteFloatingBar(),
       ),
+    );
+  }
+
+  void _showExitQuestion(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (ctx) {
+        return SimpleDialog(
+          backgroundColor: Theme.of(context).backgroundColor,
+          children: [
+            Text(
+              'Czy napewno chesz wyjśc bez zapisywania danych?',
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    "Wyjdź",
+                    style: TextStyle(
+                      color: Theme.of(context).accentColor,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    "Zapisz",
+                    style: TextStyle(
+                      color: Theme.of(context).accentColor,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        );
+      },
     );
   }
 }
