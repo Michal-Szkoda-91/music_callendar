274d3057721a9d4638b4a75691e9d9469ee622bd
Michal Szkoda <78780@stud.uz.zgora.pl>
2021-09-10 19:53:11 +0200

    poprawiono dialog aktualizacji, dodajac wersje wielojezykowa

--- lib/ads/ad_helper.dart
+++ lib/ads/ad_helper.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitID {
     if (Platform.isAndroid) {
-      // return "ca-app-pub-6198840177908447/1200786167";   //Original
-      return 'ca-app-pub-3940256099942544/6300978111'; //Test
+      return "ca-app-pub-6198840177908447/1200786167"; //Original
+      // return 'ca-app-pub-3940256099942544/6300978111'; //Test
     } else if (Platform.isIOS) {
       return "ca-app-pub-6198840177908447/1200786167";
     } else {
